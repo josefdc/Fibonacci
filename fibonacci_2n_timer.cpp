e4088789c16faa438c09792ef796858d2a1416f5
Jose Duarte <josepipe0909@gmail.com>
2023-08-17 11:47:16 -0500

    New branch, testing-timings

--- fibonacci_2n_timer.cpp
+++ fibonacci_2n_timer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <vector>
 
 // Exponential time complexity Fibonacci function
 int fibonacci(int n) {
@@ -11,18 +12,18 @@ int fibonacci(int n) {
 }
 
 int main() {
-    int n;
-    std::cout << "Enter the value of n: ";
-    std::cin >> n;
+    std::vector<int> testValues = {5, 10, 15, 20, 25, 30, 35, 40};  // Recommended values for 2^n complexity
 
-    auto start = std::chrono::high_resolution_clock::now();  // Record the start time
+    for (int n : testValues) {
+        auto start = std::chrono::high_resolution_clock::now();  // Record the start time
 
-    std::cout << "Fibonacci(" << n << ") = " << fibonacci(n) << std::endl;
+        std::cout << "Fibonacci(" << n << ") = " << fibonacci(n) << std::endl;
 
-    auto end = std::chrono::high_resolution_clock::now();    // Record the end time
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        auto end = std::chrono::high_resolution_clock::now();    // Record the end time
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
-    std::cout << "Execution time: " << duration.count() << " microseconds" << std::endl;
+        std::cout << "Execution time for n=" << n << ": " << duration.count() << " microseconds" << std::endl << std::endl;
+    }
 
     return 0;
 }
