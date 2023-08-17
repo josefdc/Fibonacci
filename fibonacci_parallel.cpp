11152a4be6c788a733a57fa520be59f9a6ea6477
Jose Duarte <josepipe0909@gmail.com>
2023-08-17 10:49:45 -0500

    paralelismo

--- fibonacci_parallel.cpp
+++ fibonacci_parallel.cpp
@@ -1,39 +1,68 @@
 #include <iostream>
 #include <vector>
-#include <omp.h>
+#include <chrono>  // Inclusión de la librería para medir tiempo
 
-std::vector<long long> fibResults;
+// Definición del tipo de matriz que contendrá números de tipo long double.
+typedef std::vector<std::vector<long double>> Matrix;
 
-long long parallelFibonacci(int n) {
+/**
+ * @brief Multiplica dos matrices y devuelve el resultado.
+ *
+ * @param a Primera matriz.
+ * @param b Segunda matriz.
+ * @return Resultado de la multiplicación de matrices.
+ */
+Matrix matrixMultiply(const Matrix &a, const Matrix &b) {
+    int n = a.size();
+    int m = b[0].size();
+    int p = b.size();
+    Matrix result(n, std::vector<long double>(m, 0));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            for (int k = 0; k < p; ++k) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+/**
+ * @brief Eleva una matriz a una potencia usando exponenciación rápida.
+ *
+ * @param mat Matriz a elevar.
+ * @param exp Exponente.
+ * @return Matriz elevada a la potencia exp.
+ */
+Matrix matrixPower(const Matrix &mat, int exp) {
+    if (exp == 1) {
+        return mat;
+    }
+    if (exp % 2 == 0) {
+        Matrix halfPower = matrixPower(mat, exp / 2);
+        return matrixMultiply(halfPower, halfPower);
+    } else {
+        Matrix halfPower = matrixPower(mat, (exp - 1) / 2);
+        return matrixMultiply(matrixMultiply(halfPower, halfPower), mat);
+    }
+}
+
+/**
+ * @brief Calcula el n-ésimo número de Fibonacci usando exponenciación de matrices.
+ *
+ * @param n Posición del número de Fibonacci a calcular.
+ * @return Número de Fibonacci en la posición n.
+ */
+long double fibonacci(int n) {
     if (n <= 1)
         return n;
 
-    if (fibResults[n] != -1)
-        return fibResults[n];
+    Matrix base = {{1, 1}, {1, 0}};
+    Matrix result = matrixPower(base, n - 1);
 
-    long long fibNminus1, fibNminus2;
-
-    #pragma omp task shared(fibNminus1)
-    fibNminus1 = parallelFibonacci(n - 1);
-
-    #pragma omp task shared(fibNminus2)
-    fibNminus2 = parallelFibonacci(n - 2);
-
-    #pragma omp taskwait
-
-    fibResults[n] = fibNminus1 + fibNminus2;
-
-    return fibResults[n];
-}
-
-long long fibonacci(int n) {
-    fibResults.assign(n + 1, -1);
-    fibResults[0] = 0;
-    fibResults[1] = 1;
-
-    #pragma omp parallel
-    #pragma omp single
-    return parallelFibonacci(n);
+    return result[0][0];
 }
 
 int main() {
@@ -41,9 +70,17 @@ int main() {
     std::cout << "Enter the value of n: ";
     std::cin >> n;
 
-    long long fibNumber = fibonacci(n);
+    // Inicio de la medición del tiempo
+    auto start = std::chrono::high_resolution_clock::now();
+
+    long double fibNumber = fibonacci(n);
+
+    // Fin de la medición del tiempo
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
     std::cout << "Fibonacci number at position " << n << ": " << fibNumber << std::endl;
+    std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
 
     return 0;
 }
